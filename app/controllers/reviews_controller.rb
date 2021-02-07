a6d6f6dd160d8ea1e53ae0ac558ed07119679aa1
Nicolas Dumont <nicolas.dumont@essca.eu>
2021-02-07 14:58:25 +0100

    Completed the most beautifulest exercise

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(params_review)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:rating, :content)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :restaurant do
-  #   resources :review, only: ["new", "update"]
-  # end
+  resources :restaurants, only: ["new", "index", "show", "create"] do
+    resources :reviews, only: ["new", "create"]
+  end
   # resources :review, only: ["destroy"]
 end
